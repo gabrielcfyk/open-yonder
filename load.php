1778fc0c04113ed49c711bb82102599e6ed35529
gabrielcfyk <gabrielcfyk@gmail.com>
2016-08-30 17:34:25 -0400

    Create load.php

---
+++ load.php
@@ -0,0 +1,6 @@
+<?php
+	//Load all the required files in order
+	require_once(dirname(__FILE__) . '/config.php');
+	require_once(dirname(__FILE__) . '/email_includes/email_db.php');
+	require_once(dirname(__FILE__) . '/email_includes/email_class.php');
+?>
