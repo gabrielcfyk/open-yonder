ada4aedd57228f3e49a781439fdac8a850502699
gabrielcfyk <gabrielcfyk@gmail.com>
2016-08-30 17:33:37 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Name of the database */
+
+define('DB_NAME', 'open_yonder');
+
+/* Name of the user */
+
+define('DB_USER', 'root');
+
+/* The user's password */
+
+define('DB_PASS', "HjC(wPgt52zZ%X3?");
+
+?>
